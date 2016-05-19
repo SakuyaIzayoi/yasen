dddbdf3ca01ad758ea6da9e88a37cfc013a230aa
Alessandro Gangemi <agangemi@stevens.edu>
2016-05-19 15:00:43 -0400

    Parse English names

--- main.go
+++ main.go
@@ -75,10 +75,11 @@ func importDB() {
 		checkErr(err)
 
 		result, err := db.Exec(
-			"INSERT INTO ship VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)",
+			"INSERT INTO ship VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26)",
 			ship.Id,
 			ship.No,
 			ship.Name.NameKanji,
+			ship.Name.NameRomaji,
 			ship.Name.Suffix,
 			ship.Stat.Fire,
 			ship.Stat.FireMax,
